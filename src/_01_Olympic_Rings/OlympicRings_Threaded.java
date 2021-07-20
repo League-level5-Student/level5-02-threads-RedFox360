125f5da6e1437fa2f1ee3e99cc683503d58ddf7e
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-07-20 16:56:06 -0700

    started level 5 module 1/2

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,22 @@
 package _01_Olympic_Rings;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One
+	// robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
+		Robot[] robots = new Robot[5];
+		for (int i = 0; i < robots.length; i++) {
+			robots[i] = new Robot((i+1) * 200, 700);
+			robots[i].penDown();
+		}
+		for (int i = 0; i < robots.length; i++) {
+			for (int k = 0; k < 360; k++) {
+				robots[1].move(2);
+				robots[1].setAngle(k);
+			}
+		}
 
+	}
 }
-
